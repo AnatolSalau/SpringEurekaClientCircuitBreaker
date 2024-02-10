e338897475efc4472cc14168eb18a600917ef4f1
AnatolSalau <salovanatoly5@mail.ru>
2024-02-10 15:48:06 +0300

    Initialize all microservices

---
+++ README.md
@@ -0,0 +1,9 @@
+### Spring EurekaClientLoadBalanced CircuitBreaker
+
+Sysout link:
+
+    https://sysout.ru/otkazoustojchivost-mikroservisov-shablon-circuit-breaker/
+
+GitHub link:
+
+    https://github.com/AnatolSalau/SpringEurekaClientCircuitBreaker
---
+++ zoo-service/src/test/java/com/example/zooservice/ZooServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zooservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooServiceApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
