008684a66f397683dd5834d537ad668d8bf66f83
AnatolSalau <salovanatoly5@mail.ru>
2024-02-10 16:27:47 +0300

    Add dependency with Animal and Ticket Dto library

--- random-animal-service/src/main/java/com/example/randomanimalservice/RandomAnimalServiceApplication.java
+++ random-animal-service/src/main/java/com/example/randomanimalservice/RandomAnimalServiceApplication.java
@@ -10,6 +10,7 @@ public class RandomAnimalServiceApplication {
 
       public static void main(String[] args) {
             SpringApplication.run(RandomAnimalServiceApplication.class, args);
+
       }
 
 }
--- ticket-service/src/main/java/com/example/ticketservice/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/example/ticketservice/TicketServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class TicketServiceApplication {
 
       public static void main(String[] args) {
+
             SpringApplication.run(TicketServiceApplication.class, args);
       }
 
