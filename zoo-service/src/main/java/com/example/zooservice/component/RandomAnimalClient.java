c73315d6b6e9eba8196f76a88484b5edf39e6c4f
AnatolSalau <salovanatoly5@mail.ru>
2024-02-11 19:59:35 +0300

    Add circuit breaker

--- zoo-service/src/main/java/com/example/zooservice/component/RandomAnimalClient.java
+++ zoo-service/src/main/java/com/example/zooservice/component/RandomAnimalClient.java
@@ -4,7 +4,9 @@ import com.example.lib.dto.AnimalDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -17,27 +19,34 @@ public class RandomAnimalClient {
       private final RestTemplate balancedRestTemplate;
       private final DiscoveryClient discoveryClient;
       private final RestTemplate restTemplate;
+      private final CircuitBreakerFactory circuitBreakerFactory;
+
 
       @Autowired
-      public RandomAnimalClient(RestTemplate balancedRestTemplate, DiscoveryClient discoveryClient, RestTemplate restTemplate) {
+      public RandomAnimalClient(RestTemplate balancedRestTemplate, DiscoveryClient discoveryClient,
+                                RestTemplate restTemplate,
+                                CircuitBreakerFactory circuitBreakerFactory) {
             this.balancedRestTemplate = balancedRestTemplate;
             this.discoveryClient = discoveryClient;
             this.restTemplate = restTemplate;
+            this.circuitBreakerFactory = circuitBreakerFactory;
       }
 
       //Balanced RestTemplate by @LoadBalanced from implementation 'org.springframework.cloud:spring-cloud-commons:4.1.1'
       //spring.application.name=random-animal есть в настройках Random Animal
       public AnimalDto randomByBalancedRestTemplate() {
             log.info("Sending  request for animal ");
-            AnimalDto animalDto = balancedRestTemplate.getForEntity("http://random-animal/random",
-                  AnimalDto.class).getBody();
+            ResponseEntity<AnimalDto> randomAnimal = circuitBreakerFactory.create("randomAnimal").run(
+                  () -> restTemplate.getForEntity("http://random-animal/random", AnimalDto.class),
+                  throwable -> fallbackRandom());
+
+            AnimalDto animalDto = randomAnimal.getBody();
             log.info("AnimalDto from RandomAnimal: {} ", animalDto);
             return animalDto;
       }
 
       public AnimalDto randomByDiscoveryClient() {
-            List<ServiceInstance> instances = discoveryClient.getInstances("random-animal");
-            System.out.println(instances);
+            log.info("Sending  request for animal ");
             ServiceInstance serviceInstance = discoveryClient.getInstances("random-animal")
                   .stream()
                   .findAny()
@@ -48,8 +57,17 @@ public class RandomAnimalClient {
             );
             String uri = uriComponentsBuilder.toUriString();
 
-            AnimalDto animalDto = restTemplate.getForEntity(uri, AnimalDto.class).getBody();
+            ResponseEntity<AnimalDto> randomAnimal = circuitBreakerFactory.create("randomAnimal").run(
+                  () -> restTemplate.getForEntity(uri, AnimalDto.class),
+                  throwable -> fallbackRandom());
+
+            AnimalDto animalDto = randomAnimal.getBody();
 
             return animalDto;
+      }
+
+      public ResponseEntity<AnimalDto> fallbackRandom() {
+            log.info("Fallback random");
+            return ResponseEntity.ok().body(new AnimalDto("no animal"));
       }
 }
--- zoo-service/src/main/java/com/example/zooservice/component/TicketClient.java
+++ zoo-service/src/main/java/com/example/zooservice/component/TicketClient.java
@@ -5,7 +5,9 @@ import com.example.lib.dto.TicketDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -17,24 +19,32 @@ public class TicketClient {
       private final DiscoveryClient discoveryClient;
       private final RestTemplate restTemplate;
 
+      private final CircuitBreakerFactory circuitBreakerFactory;
+
       @Autowired
-      public TicketClient(RestTemplate balancedRestTemplate, DiscoveryClient discoveryClient, RestTemplate restTemplate) {
+      public TicketClient(RestTemplate balancedRestTemplate, DiscoveryClient discoveryClient, RestTemplate restTemplate,
+                          CircuitBreakerFactory circuitBreakerFactory) {
             this.balancedRestTemplate = balancedRestTemplate;
             this.discoveryClient = discoveryClient;
             this.restTemplate = restTemplate;
+            this.circuitBreakerFactory = circuitBreakerFactory;
       }
 
       //Balanced RestTemplate by @LoadBalanced from implementation 'org.springframework.cloud:spring-cloud-commons:4.1.1'
       //spring.application.name=random-animal есть в настройках Random Animal
       public TicketDto randomByBalancedRestTemplate() {
-            log.info("Sending  request for animal ");
-            TicketDto ticketDto = balancedRestTemplate.getForEntity("http://random-ticket/",
-                  TicketDto.class).getBody();
+            log.info("Sending  request for ticket ");
+            ResponseEntity<TicketDto> randomAnimal = circuitBreakerFactory.create("randomTicket").run(
+                  () -> restTemplate.getForEntity("http://random-ticket/", TicketDto.class),
+                  throwable -> fallbackRandom());
+
+            TicketDto ticketDto = randomAnimal.getBody();
             log.info("TicketDto from Ticket service: {} ", ticketDto);
             return ticketDto;
       }
 
       public TicketDto randomByDiscoveryClient() {
+            log.info("Sending  request for ticket ");
             ServiceInstance serviceInstance = discoveryClient.getInstances("random-ticket")
                   .stream()
                   .findAny()
@@ -45,8 +55,17 @@ public class TicketClient {
             );
             String uri = uriComponentsBuilder.toUriString();
 
-            TicketDto ticketDto = restTemplate.getForEntity(uri, TicketDto.class).getBody();
+            ResponseEntity<TicketDto> randomAnimal = circuitBreakerFactory.create("randomTicket").run(
+                  () -> restTemplate.getForEntity(uri, TicketDto.class),
+                  throwable -> fallbackRandom());
+
+            TicketDto ticketDto = randomAnimal.getBody();
+            log.info("TicketDto from Ticket service: {} ", ticketDto);
 
             return ticketDto;
+      }
+      public ResponseEntity<TicketDto> fallbackRandom() {
+            log.info("Fallback random");
+            return ResponseEntity.ok().body(new TicketDto("no ticket"));
       }
 }
